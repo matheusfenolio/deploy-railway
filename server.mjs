f60ea6afbbff6b2743b0cd458feb96b6c2f9d094
Matheus Prado <matheusfenoliodoprado@outlook.com>
2022-02-23 21:54:04 -0300

    Testing webhook

--- server.mjs
+++ server.mjs
@@ -4,9 +4,11 @@ import PrismaClient from '@prisma/client'
 const server = express();
 const prisma = new PrismaClient.PrismaClient()
 
-server.get('/', async (req, res) => prisma.person.findMany()
-.then(response => res.status(200).json(response))
-.catch(error => res.status(500).json(error)) 
+server.get('/', async (req, res) => 
+    prisma.person
+    .findMany()
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(500).json(error)) 
 );
 
 server.listen(process.env.PORT, () => console.info(`Server is up and listening at ${process.env.PORT}`));
